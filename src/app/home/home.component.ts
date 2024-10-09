a2f342e7c1dc2da11afb54455b405b057385249d
Youssef-joe <joe92228@gmail.com>
2024-10-10 01:55:27 +0300

    updating home page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProductComponent } from '../components/product/product.component';
 import { CommonModule } from '@angular/common';
 import { Paginator, PaginatorModule } from 'primeng/paginator';
 import { EditPopupComponent } from '../components/edit-popup/edit-popup.component';
 import { ButtonModule } from 'primeng/button';
+import { HttpClient, HttpClientModule, HttpParams } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -13,12 +14,36 @@ import { ButtonModule } from 'primeng/button';
     CommonModule,
     PaginatorModule,
     EditPopupComponent,
-    ButtonModule
+    ButtonModule,
+    HttpClientModule,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-  constructor() {}
+export class HomeComponent implements OnInit {
+  constructor(private http: HttpClient) {}
+  newArrivals = [{ name: '', price: '', image: '' }];
 
+  featuredProducts: any[] = [];
+  featuredProductsLimit: number = 6;
+
+  ngOnInit(): void {
+    this.fetchProducts();
+  }
+
+  fetchProducts(): void {
+    const apiUrl = 'https://api.escuelajs.co/api/v1/products';
+    let params = new HttpParams().set(
+      'limit',
+      this.featuredProductsLimit.toString()
+    );
+    this.http.get<any[]>(apiUrl, { params: params }).subscribe(
+      (data) => {
+        this.featuredProducts = data.slice(0, this.featuredProductsLimit);
+      },
+      (error) => {
+        console.error('featured products error:', error);
+      }
+    );
+  }
 }
